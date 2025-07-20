d41180d66c70b604841acf956334d3a71d59e07e
ramnaresh-ahi <ramnareshahi77@gmail.com>
2025-07-20 13:14:14 +0530

    Documentations are going to public

--- tests/valid_email.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT *
-FROM {{ ref('stg_students') }}
-WHERE email IS NOT NULL
-  AND NOT REGEXP_CONTAINS(
-    CAST(email AS STRING),
-    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-  )
